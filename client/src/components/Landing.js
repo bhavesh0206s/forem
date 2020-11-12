3e5cf90595f2420232bde0757fe666782e8f9d6a
bhavesh0206s <bhaveshsuthar0299@gmail.com>
2020-11-13 00:25:49 +0530

    added basic layout

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,7 +1,146 @@
+import { useState } from 'react';
+import { Layout, Affix, Button, Image,  Menu, Icon, Breadcrumb } from 'antd';
+import { useMediaQuery } from 'react-responsive'
+import Navbar from './navbar';
+import './landing.css'
+import {
+  AppstoreOutlined,
+  BarChartOutlined,
+  CloudOutlined,
+  ShopOutlined,
+  TeamOutlined,
+  UserOutlined,
+  UploadOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+
+const { SubMenu } = Menu;
+const { Header, Content, Footer, Sider } = Layout;
+
 const Landing = () => {
+
+  const [collapsed, setCollapsed] = useState(false);
+  const [contentLeftMargin, setContentLeftMargin] = useState(200);
+  const isTabletOrMobileDevice = useMediaQuery({
+    query: '(max-device-width: 1224px)'
+  });
+  const onCollapse = collapsed => {
+    setCollapsed(collapsed);
+    if(collapsed){
+      setContentLeftMargin(80)
+    }else{
+      setContentLeftMargin(200)
+    }
+  }
+
   return (
-    <div>
-      Helel
+    <div >
+      <Affix offsetTop={0}>
+        <Navbar />
+      </Affix>  
+      <Layout>
+        {isTabletOrMobileDevice ? (
+          <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+              <Menu.Item key="1">nav 1</Menu.Item>
+              <Menu.Item key="2">nav 2</Menu.Item>
+              <Menu.Item key="3">nav 3</Menu.Item>
+            </Menu>
+          </Header>
+        ): (
+        <Sider
+          style={{
+            overflow: 'auto',
+            height: '100vh',
+            position: 'fixed',
+            left: 0,
+          }}
+          collapsible
+          collapsed={collapsed}
+          onCollapse={onCollapse}
+        >
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+            <Menu.Item key="1" icon={<UserOutlined />}>
+              nav 1
+            </Menu.Item>
+            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+              nav 2
+            </Menu.Item>
+            <Menu.Item key="3" icon={<UploadOutlined />}>
+              nav 3
+            </Menu.Item>
+            <Menu.Item key="4" icon={<BarChartOutlined />}>
+              nav 4
+            </Menu.Item>
+            <Menu.Item key="5" icon={<CloudOutlined />}>
+              nav 5
+            </Menu.Item>
+            <Menu.Item key="6" icon={<AppstoreOutlined />}>
+              nav 6
+            </Menu.Item>
+            <Menu.Item key="7" icon={<TeamOutlined />}>
+              nav 7
+            </Menu.Item>
+            <Menu.Item key="8" icon={<ShopOutlined />}>
+              nav 8
+            </Menu.Item>
+            <Menu.Item key="8" icon={<ShopOutlined />}>
+              nav 8
+            </Menu.Item>
+            <Menu.Item key="8" icon={<ShopOutlined />}>
+              nav 8
+            </Menu.Item>
+            <Menu.Item key="8" icon={<ShopOutlined />}>
+              nav 8
+            </Menu.Item>
+            <Menu.Item key="8" icon={<ShopOutlined />}>
+              nav 8
+            </Menu.Item>
+            <Menu.Item key="8" icon={<ShopOutlined />}>
+              nav 8
+            </Menu.Item>
+            <Menu.Item key="8" icon={<ShopOutlined />}>
+              nav 8
+            </Menu.Item>
+            <Menu.Item key="8" icon={<ShopOutlined />}>
+              nav 8
+            </Menu.Item>
+            <Menu.Item key="8" icon={<ShopOutlined />}>
+              nav 8
+            </Menu.Item>
+            <Menu.Item style={{paddingBottom: 150}} key="9" icon={<ShopOutlined />}>
+              nav 9
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        )}
+        <Layout className="site-layout" style={{ marginLeft: contentLeftMargin }}>
+          <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+            <div className="site-layout-background" style={{ padding: 24 }}>
+              ...
+              <br />
+              Really
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              ...
+              <br />
+              long
+
+              <br />
+              ...
+              <br />
+              ...
+
+              content
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+        </Layout>
+      </Layout>
+      <Footer>Footer</Footer>
     </div>
   );
 }
---
+++ client/src/components/navbar/index.js
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import LeftMenu from './LeftMenu'
+import { useMediaQuery } from 'react-responsive'
+import RightMenu from './RightMenu'
+import { Drawer, Button, Input } from 'antd';
+import './nav.css';
+import mainLogo from '../../assets/logo_transparent.png'
+
+const { Search } = Input;
+const Navbar = () => {
+  const [current, setCurrent] = useState('mail');
+  const [visible, setVisible] = useState(false); 
+
+  const isTabletOrMobileDevice = useMediaQuery({
+    query: '(max-device-width: 1224px)'
+  });
+
+  const showDrawer = () => {
+    setVisible(true)
+  };
+  
+  const onClose = () => {
+    setVisible(false)
+  };
+
+  const onSearch = () => {
+
+  }
+
+  return (
+    <nav className="menuBar">
+      <div className="logo">
+        <a href="">forem</a>
+        <div className='searcbBar'>
+          {isTabletOrMobileDevice && (
+            <Search
+              placeholder="input search text"
+              allowClear
+              enterButton="Search"
+              size="large"
+              onSearch={onSearch}
+              style={{width: 100}}
+            />
+          )}
+        </div>
+      </div>
+      <div className="menuCon">
+        {/* <div className="leftMenu">
+          <LeftMenu />
+        </div> */}
+        <div className="rightMenu">
+          <RightMenu />
+        </div>
+        <Button className="barsMenu" type="primary" onClick={showDrawer}>
+          <span className="barsBtn"></span>
+        </Button>
+        <Drawer
+          title="Basic Drawer"
+          placement="right"
+          closable={false}
+          onClose={onClose}
+          visible={visible}
+        >
+          <RightMenu />
+        </Drawer>
+      </div>
+    </nav>
+  );
+}
+ 
+export default Navbar;
