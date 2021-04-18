4281c097367edb94314353e8cc1e6372f269d6db
bhavesh0206s <bhaveshsuthar0299@gmail.com>
2021-04-18 18:11:09 +0530

    .gitignore is now working

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  googleClientID: '1083414167123-5ehcg3k81a1oc7chl6ftvoovmgtooq7f.apps.googleusercontent.com',
-  googleClentSecret: 'tw51y3jtoRWBn8fgod46-bPK',
-  mongoURI: 'mongodb+srv://forem:forem@cluster0.kkewz.mongodb.net/<dbname>?retryWrites=true&w=majority',
-}
